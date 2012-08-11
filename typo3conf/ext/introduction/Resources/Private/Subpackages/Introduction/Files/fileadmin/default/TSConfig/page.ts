ac0114876ec94d69277d534935db2ed62955b2de
Philipp Gampe <dev@philippgampe.info>
2012-08-11 15:48:16 +0200

    [BUGFIX] Extract TSconfig of record folders
    Change-Id: I9bfdcda5453f7dee49a0f20a0fcce987e1924f43
    Fixes: #39707
    Releases: 6.0

--- typo3conf/ext/introduction/Resources/Private/Subpackages/Introduction/Files/fileadmin/default/TSConfig/page.ts
+++ typo3conf/ext/introduction/Resources/Private/Subpackages/Introduction/Files/fileadmin/default/TSConfig/page.ts
@@ -50,3 +50,28 @@ TCEMAIN {
 	group = show,edit,delete,new,editcontent
 	everybody =
 }
+
+# Condition for news storage folder
+[PIDinRootline = 18]
+	mod.web_list {
+		# limit the creation of new records in this sysFolder to these types
+		allowedNewTables = tt_news,tt_news_cat,sys_note
+	}
+
+	# This will open the news singleView page (id 23) when clicking 'preview' for a news record
+	tx_ttnews.singlePid = 23
+
+	TCEMAIN {
+		# Clear cache of the News page after content of the News folder has changed
+		clearCacheCmd = 23
+		clearCache_pageSiblingChildren = 1
+	}
+[END]
+
+# Condition for frontend user storage folder
+[PIDinRootline = 29]
+	mod.web_list {
+		# limit the creation of new records in this sysFolder to these types
+		allowedNewTables = fe_groups,fe_users,sys_note
+	}
+[END]
