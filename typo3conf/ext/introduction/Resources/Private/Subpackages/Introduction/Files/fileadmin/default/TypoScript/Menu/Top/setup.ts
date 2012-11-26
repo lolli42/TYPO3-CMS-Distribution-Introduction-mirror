53a7ae8ff6e310be5b8cf92641d702b99e0df7f0
Bernhard Kraft <kraftb@think-open.at>
2012-11-26 18:17:33 +0100

    [BUGFIX] Reference loop in menu TypoScript
    The base menu configuration was put to the TypoScript object
    "menu" and other objects like "menu.top" just copied and
    extended from "menu". This way, each copy process extended
    the base "menu" object and created internal reference loops.
    
    Change-Id: Ie640a4f76bbe7ba9eb319ac063bd28322ebc5126
    Fixes: #42561
    Releases: 4.5, 4.6, 4.7, 6.0

--- typo3conf/ext/introduction/Resources/Private/Subpackages/Introduction/Files/fileadmin/default/TypoScript/Menu/Top/setup.ts
+++ typo3conf/ext/introduction/Resources/Private/Subpackages/Introduction/Files/fileadmin/default/TypoScript/Menu/Top/setup.ts
@@ -5,7 +5,7 @@ Configuration for the top right menu.
 */
 
 # Copy basic menu object from TS template 'menu' so we don't have to repeat all that
-menu.top < menu
+menu.top < menu.template
 menu.top {
 	# Also show pages which have not-in-menu flag set in page properties
 	includeNotInMenu = 1
